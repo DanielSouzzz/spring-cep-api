5ad907f78a5d87c507d9d2003d31179fddee03c2
danielsouzzz <danielsouzanovo@gmial.com>
2024-10-01 18:43:50 -0300

    refactor.

--- src/main/java/com/project/viacep/controller/ControllerCep.java
+++ src/main/java/com/project/viacep/controller/ControllerCep.java
@@ -2,17 +2,17 @@ package com.project.viacep.controller;
 
 import com.project.viacep.model.dto.CepDTO;
 import com.project.viacep.service.CepService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/cep")
 public class ControllerCep {
-    @Autowired
-    private CepService cepService;
+    private final CepService cepService;
 
     @GetMapping("/{cep}")
     public CepDTO getCep(@PathVariable("cep") String cep) {
