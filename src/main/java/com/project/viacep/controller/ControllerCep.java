81832959b8e9119ee328d5ea06664b7bbb70832d
danielsouzzz <danielsouzanovo@gmial.com>
2024-10-03 01:28:50 -0300

    Interface desenvolvida.

--- src/main/java/com/project/viacep/controller/ControllerCep.java
+++ src/main/java/com/project/viacep/controller/ControllerCep.java
@@ -3,14 +3,12 @@ package com.project.viacep.controller;
 import com.project.viacep.model.dto.CepDTO;
 import com.project.viacep.service.CepService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/cep")
+@CrossOrigin("*")
 public class ControllerCep {
     private final CepService cepService;
 
